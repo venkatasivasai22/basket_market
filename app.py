f801d60130f85bff7ae578d266ab65a7c94eebcb
venkatasivasai22 <venkat.musam@gmail.com>
2024-04-03 09:38:34 +0530

    marketbasket

--- app.py
+++ app.py
@@ -1,28 +1,43 @@
+import streamlit as st
 import pandas as pd
 import joblib
-import streamlit as st
 
+# Load association rules
 Rules = joblib.load('association_rules.pkl')
 
-def recommend_items(rules, input_item):
-    filtered_rules = rules[rules['antecedents'].apply(lambda x: input_item in x)]
-    filtered_rules = filtered_rules.sort_values(by=['lift', 'confidence', 'support'], ascending=False)
-    recommended_items = filtered_rules['consequents'].tolist()
-    return set(recommended_items[0]) if recommended_items else set()
+def recommend_items(rules, input_items):
+    recommended_items = set()
+    
+    for input_item in input_items:
+        # Filter rules for the given input item in antecedents
+        filtered_rules = rules[rules['antecedents'].apply(lambda x: input_item in x)]
+        
+        if not filtered_rules.empty:
+            # Sort rules by lift, confidence, and support in descending order
+            filtered_rules = filtered_rules.sort_values(by=['lift', 'confidence', 'support'], ascending=False)
+            
+            # Extract recommended items from consequents column
+            recommended_items.update(filtered_rules['consequents'].iloc[0])
+    
+    return recommended_items
 
 def load_association_rules():
     return Rules
 
-def main():
-    rules = load_association_rules()
+# Load association rules
+rules = load_association_rules()
 
-    st.title("Item Recommender")
+# Streamlit app
+st.title('Item Recommendation System')
 
-    user_input = st.text_input("Enter an item:")
+# User input
+user_input = st.text_input('Enter the items you are interested in (separated by commas):')
 
-    if st.button("Recommend"):
-        recommended_items = recommend_items(rules, user_input)
-        st.write(f"Recommended items for {user_input}: {recommended_items}")
+if st.button('Recommend') and user_input:
+    input_items = [item.strip() for item in user_input.split(',')]
+    
+    # Get recommended items based on user input
+    recommended_items = recommend_items(rules, input_items)
+    
+    st.write("Recommended items:", recommended_items)
 
-if __name__ == "__main__":
-    main()
